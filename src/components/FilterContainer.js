b523ba8c8698647cd8fb8632e157595fb0c03a42
Gabriel <gabriel@Gabriels-MacBook-Air.local>
2020-05-03 18:50:22 -0400

    wip site redesign midnight memos

---
+++ src/components/FilterContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class FilterContainer extends Component {
+  constructor(props) {
+    super(props);
+
+    // this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="FilterContainerDiv" className="filter-container">
+        <div className="section">
+          {/* React-collapsible goes here... */}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default FilterContainer
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import logo from '../img/logo.svg'
+import logo from '../img/midnight-memos.svg'
 import facebook from '../img/social/facebook.svg'
 import instagram from '../img/social/instagram.svg'
 import twitter from '../img/social/twitter.svg'
@@ -10,18 +10,18 @@ import vimeo from '../img/social/vimeo.svg'
 const Footer = class extends React.Component {
   render() {
     return (
-      <footer className="footer has-background-black has-text-white-ter">
-        <div className="content has-text-centered">
+      <footer className="footer has-text-white-ter">
+        <div className="content footer-logo has-text-centered">
           <img
             src={logo}
             alt="Kaldi"
-            style={{ width: '14em', height: '10em' }}
+            style={{ width: '25em', height: '5em', 'filter': 'invert(1)' }}
           />
         </div>
-        <div className="content has-text-centered has-background-black has-text-white-ter">
-          <div className="container has-background-black has-text-white-ter">
-            <div className="columns">
-              <div className="column is-4">
+        <div className="content has-text-centered has-text-white-ter">
+          <div className="container has-text-white-ter">
+            <div className="grid-columns">
+              <div className="is-4">
                 <section className="menu">
                   <ul className="menu-list">
                     <li>
@@ -57,7 +57,7 @@ const Footer = class extends React.Component {
                   </ul>
                 </section>
               </div>
-              <div className="column is-4">
+              <div className="is-4">
                 <section>
                   <ul className="menu-list">
                     <li>
@@ -73,7 +73,7 @@ const Footer = class extends React.Component {
                   </ul>
                 </section>
               </div>
-              <div className="column is-4 social">
+              <div className="is-4 social">
                 <a title="facebook" href="https://facebook.com">
                   <img
                     src={facebook}
